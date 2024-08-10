ce681905651e313068a070d5c758934c30a03a43
Sarvinoz0112 <maxmadaliyevasarvinoz@gmail.com>
2024-08-10 15:11:38 +0500

    see_all_orders function added

--- admin.py
+++ admin.py
@@ -1,17 +1,27 @@
-from file_manager import JsonManager, admin_manager
+from file_manager import JsonManager, admin_manager, order_manager
+
 
 def see_all_users():
     user_manager = JsonManager("users.json")
     users = user_manager.read_data()
 
     if not users:
-        print("There is no any users")
+        print("No users found.")
     else:
         print("\nList of all registered users:")
-        print("Username                 | Email")
-        print("═══════════════════════════════════════════")
+        print("{:<20} {:<30}".format("Ism", "Email"))
+        print("-" * 50)
         for user in users:
-            print(f"{user['user_name']} | {user['email']}")
+            print("{:<20} {:<30}".format(user['user_name'], user['email']))
+
+
+def print_package():
+    data = admin_manager.read_data()
+    print(f"Admin Name: {data['admin_name']}")
+    print("Packages:")
+
+    for package in data['packages']:
+        print(f"  - Range: {package['package_range']}, Price: {package['price']}")
 
 
 def create_water_package():
@@ -33,3 +43,24 @@ def create_water_package():
     }
 
     admin_manager.write_data(admin)
+    print_package()
+
+
+def see_all_orders():
+    orders = order_manager.read_data()
+
+    if not orders:
+        print("No orders found.")
+    else:
+        print("\nUser Orders:")
+        print("{:<30} {:<30}".format("Email", "Orders"))
+        print("-" * 80)
+        for user in orders:
+            user_orders = user['orders']
+
+            if len(user_orders) > 0:
+                orders = ", ".join(user_orders)
+            else:
+                print("No orders")
+            
+            print("{:<30} {:<30}".format(user['email'], orders))
