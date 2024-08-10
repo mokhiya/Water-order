4f460ca39ecef64dc82b62d23f9f37e9445eb2da
Sarvinoz0112 <maxmadaliyevasarvinoz@gmail.com>
2024-08-10 15:28:07 +0500

    add comment

--- admin.py
+++ admin.py
@@ -1,9 +1,10 @@
-from file_manager import JsonManager, admin_manager, order_manager
-
+from file_manager import JsonManager, admin_manager, order_manager, user_manager
 
 
 def see_all_users():
-    user_manager = JsonManager("users.json")
+    """
+    Fetches and displays all registered users from 'users.json'.
+    """
     users = user_manager.read_data()
 
     if not users:
@@ -12,23 +13,26 @@ def see_all_users():
         print("\nList of all registered users:")
         print("{:<20} {:<30}".format("Ism", "Email"))
         print("-" * 50)
-        print("{:<20} {:<30}".format("Ism", "Email"))
-        print("-" * 50)
         for user in users:
             print("{:<20} {:<30}".format(user['user_name'], user['email']))
 
 
-
 def print_package():
+    """
+    Prints the name of the admin and the available water packages with their ranges and prices.
+    """
     data = admin_manager.read_data()
     print(f"Admin Name: {data['admin_name']}")
     print("Packages:")
-
+    
     for package in data['packages']:
         print(f"  - Range: {package['package_range']}, Price: {package['price']}")
 
 
 def create_water_package():
+    """
+    Prompts the admin to create new water packages and saves them to 'admin_data'.
+    """
     admin_data = []
     package_quantity = int(input("How many packages do you want to create? "))
 
@@ -51,6 +55,9 @@ def create_water_package():
 
 
 def see_all_orders():
+    """
+    Fetches and displays all user orders from 'orders.json'.
+    """
     orders = order_manager.read_data()
 
     if not orders:
@@ -61,10 +68,5 @@ def see_all_orders():
         print("-" * 80)
         for user in orders:
             user_orders = user['orders']
-
-            if len(user_orders) > 0:
-                orders = ", ".join(user_orders)
-            else:
-                print("No orders")
-            
-            print("{:<30} {:<30}".format(user['email'], orders))
+            order_list = ", ".join(user_orders) if user_orders else "No orders"
+            print("{:<30} {:<30}".format(user['email'], order_list))
